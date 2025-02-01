5b9f299536efc0d73ac0aca70481295ee3d6e67e
Celena Valenzuela <celenaisabelle@outlook.com>
2025-02-01 18:37:20 -0500

    welcome message and userinput in main.

--- main/Main.java
+++ main/Main.java
@@ -6,7 +6,12 @@ import util.*;
 public class Main {
 
     public static void main(String[] args) {
-	
+
+        //print out welcome message and ask for input.
+        System.out.println("Welcome to my grade book!");
+        System.out.println("Please enter the information of the first student using the following format: ");
+        System.out.println("\"firstName lastName PID grade\"");
+        System.out.println("Press enter when you are done.");
     }
 
 }
