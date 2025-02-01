ee44094f7b3976a56280b1ced5b736590ac62457
Celena Valenzuela <celenaisabelle@outlook.com>
2025-02-01 18:20:03 -0500

    added project main files and util

---
+++ main/Main.java
@@ -0,0 +1,12 @@
+/* This is part of the starter code! 
+ * You need to complete this class yourself!*/
+package main;
+import util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	
+    }
+
+}
